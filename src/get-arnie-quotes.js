8365d99c46052ea4d7193af5d94711fc8aabf0ee
minhhoang <minhcntt@gmail.com>
2026-04-23 12:57:08 +1000

    feat :: Implement mapTheResponsesToTheResults helper function - Improve readability - Parse body Json and map status 200 to Arnie Quote; else FAILURE

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,17 @@
 const { httpGet } = require('./mock-http-interface');
 
+const mapTheResponsesToTheResults = (response) => {
+  const { status, body } = response;
+  const { message } = JSON.parse(body);
+  return status === 200 
+    ? { 'Arnie Quote': message } 
+    : { 'FAILURE': message };
+};
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
- 
   const res = await Promise.all(urls.map(url => httpGet(url)));
-  const results = [];
+  const results = res.map(mapTheResponsesToTheResults);
+  console.log(results);
   return results;
 };
 
