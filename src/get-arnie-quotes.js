ba51d6a77cf9ca2245ea694c6ab9c442e54e68ad
minhhoang <minhcntt@gmail.com>
2026-04-23 12:52:02 +1000

    feat :: Implement getArnieQuotes function - Use Promise.all for concurrent http requests - Preserve input url order in results array

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -2,7 +2,10 @@ const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
-  // return results;
+ 
+  const res = await Promise.all(urls.map(url => httpGet(url)));
+  const results = [];
+  return results;
 };
 
 module.exports = {
